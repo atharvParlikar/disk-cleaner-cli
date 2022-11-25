89a95dbbe69d0c47b12f184f8fd3d3de8145664d
atharvParlikar <atharvparlikar@gmail.com>
2022-11-25 19:31:00 +0530

    created get hashmap function

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,49 @@
 #![allow(warnings, unused)]
+use core::panic;
 use sha256::digest;
 use std::collections::HashMap;
 use std::fs::{self, metadata};
 use std::path::PathBuf;
 
-fn print_tree(paths: Vec<PathBuf>) -> Vec<PathBuf> {
-    for path in paths {
-        let metadata = metadata(&path).unwrap();
-        if metadata.is_dir() {
-            let dir_contents = fs::read_dir(&path).unwrap();
-            let mut new_vec: Vec<PathBuf> = Vec::new();
-            for i in dir_contents {
-                new_vec.push(i.expect("unable to get path").path());
-            }
-            print_tree(new_vec);
+fn get_files(path: PathBuf) -> Vec<PathBuf> {
+    let mut files: Vec<PathBuf> = Vec::new();
+    for x in fs::read_dir(path).unwrap() {
+        let path_ = x.unwrap().path();
+        let meta = metadata(&path_).unwrap();
+        if meta.is_file() {
+            files.push(path_.clone());
         }
-        if metadata.is_file() {
-            println!("{:?}", path);
+        if meta.is_dir() {
+            let files_ = get_files(path_);
+            for file in files_ {
+                files.push(file);
+            }
         }
     }
-    let mut test: Vec<PathBuf> = Vec::new();
-    test.push(PathBuf::new());
-    return test;
+    return files;
+}
+
+fn get_hash_obj(path: PathBuf) -> HashMap<PathBuf, String> {
+    let mut fileHash: HashMap<PathBuf, String> = HashMap::new();
+    for i in get_files(path) {
+        let mut compatable: bool = match fs::read_to_string(&i) {
+            Ok(file) => true,
+            Err(error) => false,
+        };
+        if compatable {
+            fileHash.insert(i.clone(), digest(fs::read_to_string(&i).unwrap()));
+        } else {
+            let bytes = fs::read(&i).unwrap();
+            fileHash.insert(i.clone(), sha256::digest_bytes(&bytes));
+        }
+    }
+    return fileHash;
 }
 
 fn main() {
-    let dir_contents = fs::read_dir(".").unwrap();
-    let mut new_vec: Vec<PathBuf> = Vec::new();
-    for i in fs::read_dir(".").unwrap() {
-        new_vec.push(i.unwrap().path());
+    let mut path: PathBuf = PathBuf::new();
+    path.push(".");
+    for (key, value) in get_hash_obj(path) {
+        println!("{}", value);
     }
-    print_tree(new_vec);
 }
