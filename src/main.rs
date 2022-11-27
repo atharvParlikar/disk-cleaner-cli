a60e5c9f7f029c348825a4b75fe5dc39e28c7e0a
atharvParlikar <atharvparlikar@gmail.com>
2022-11-27 23:50:50 +0530

    added logic to skip hashing large files (<5MB)

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use sha256::digest;
 use std::collections::HashMap;
 use std::fs::{self, metadata};
 use std::io::stdin;
-use std::path::PathBuf;
+use std::path::{self, PathBuf};
 
 fn get_files(path: PathBuf) -> Vec<PathBuf> {
     let mut files: Vec<PathBuf> = Vec::new();
@@ -26,12 +26,12 @@ fn get_files(path: PathBuf) -> Vec<PathBuf> {
 
 fn get_hash_obj(path: PathBuf) -> HashMap<PathBuf, String> {
     let mut fileHash: HashMap<PathBuf, String> = HashMap::new();
-    for i in get_files(path) {
+    for i in get_files(path.clone()) {
         let mut compatable: bool = match fs::read_to_string(&i) {
             Ok(file) => true,
             Err(error) => false,
         };
-        if compatable {
+        if compatable && get_file_size(i.clone()) < 5000000 {
             fileHash.insert(i.clone(), digest(fs::read_to_string(&i).unwrap()));
         } else {
             let bytes = fs::read(&i).unwrap();
@@ -41,14 +41,18 @@ fn get_hash_obj(path: PathBuf) -> HashMap<PathBuf, String> {
     return fileHash;
 }
 
+fn get_file_size(path: PathBuf) -> u64 {
+    let meta = metadata(path).unwrap();
+    return meta.len();
+}
+
 fn main() {
     let mut path: PathBuf = PathBuf::new();
     let mut path_string = String::new();
     stdin()
         .read_line(&mut path_string)
-        .ok()
         .expect("failed to read the line");
-    path.push(path_string);
+    path.push(&path_string[..path_string.len() - 1]);
     for (key, value) in get_hash_obj(path) {
         println!("{}", value);
     }
